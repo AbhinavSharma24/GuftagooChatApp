5eec72f47ee90a5f24ff6cd5a4b3a3bf463b17b6
abhinav <abhinavsharma24999@gmail.com>
2019-08-26 01:34:58 +0530

    All Users Display done

---
+++ app/src/main/java/com/example/guftagoochatapp/Adapter/UserAdapter.java
@@ -0,0 +1,66 @@
+package com.example.guftagoochatapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.guftagoochatapp.Model.User;
+import com.example.guftagoochatapp.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mUser;
+
+    public UserAdapter(Context mContext, List<User> mUser){
+        this.mContext = mContext;
+        this.mUser = mUser;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item,parent,false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = mUser.get(position);
+        holder.username.setText(user.getUsername());
+        if(user.getImageURL().equals("default")){
+            holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+        } else{
+            Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUser.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView username;
+        public CircleImageView profile_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+        }
+    }
+
+}
--- app/src/main/java/com/example/guftagoochatapp/Fragments/UsersFragment.java
+++ app/src/main/java/com/example/guftagoochatapp/Fragments/UsersFragment.java
@@ -4,22 +4,78 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.guftagoochatapp.Adapter.UserAdapter;
+import com.example.guftagoochatapp.Model.User;
 import com.example.guftagoochatapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsersFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private List<User> mUser;
+    private UserAdapter userAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mUser = new ArrayList<>();
+        readUser();
+
+        return view;
+    }
+
+    private void readUser() {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUser.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+
+                    assert user != null;
+                    assert firebaseUser != null;
+                    if(!user.getId().equals(firebaseUser.getUid())){
+                        mUser.add(user);
+                    }
+                }
+
+                userAdapter = new UserAdapter(getContext(), mUser);
+                recyclerView.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
