34e4f33a4664a794025af62744123e56ad98e480
abhinav <abhinavsharma24999@gmail.com>
2019-08-29 19:41:40 +0530

    Oreo Notifications sending done

--- app/src/main/java/com/example/guftagoochatapp/Notifications/MyFirebaseMessaging.java
+++ app/src/main/java/com/example/guftagoochatapp/Notifications/MyFirebaseMessaging.java
@@ -1,11 +1,13 @@
 package com.example.guftagoochatapp.Notifications;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.guftagoochatapp.MessageActivity;
@@ -25,9 +27,42 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
         String sented = remoteMessage.getData().get("sented");
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        if(firebaseUser != null && sented.equals(firebaseUser.getUid())){
-            sendNotification(remoteMessage);
+        if(firebaseUser != null && sented.equals(firebaseUser.getUid())) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                sendOreoNotification(remoteMessage);
+            } else {
+                sendNotification(remoteMessage);
+            }
         }
+    }
+
+    private void sendOreoNotification(RemoteMessage remoteMessage) {
+        String user = remoteMessage.getData().get("user");
+        String icon = remoteMessage.getData().get("icon");
+        String title = remoteMessage.getData().get("title");
+        String body = remoteMessage.getData().get("body");
+
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+
+        int j = Integer.parseInt(user.replaceAll("[\\D]", ""));
+        Intent intent = new Intent(this, MessageActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("userid", user);
+        intent.putExtras(bundle);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, j, intent, PendingIntent.FLAG_ONE_SHOT);
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        OreoNotifications oreoNotifications = new OreoNotifications(this);
+        Notification.Builder builder = oreoNotifications.getOreoNotification(title,body,icon,pendingIntent,defaultSound);
+
+        int i = 0;
+        if(j > 0){
+            i = j;
+        }
+
+        oreoNotifications.getManager().notify(i, builder.build());
     }
 
     private void sendNotification(RemoteMessage remoteMessage) {
---
+++ app/src/main/java/com/example/guftagoochatapp/Notifications/OreoNotifications.java
@@ -0,0 +1,59 @@
+package com.example.guftagoochatapp.Notifications;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.net.Uri;
+import android.os.Build;
+
+public class OreoNotifications extends ContextWrapper {
+
+    private static final String CHANNEL_ID = "com.example.guftagoochatapp";
+    private static final String CHANNEL_NAME = "guftagoochatapp";
+
+    private NotificationManager notificationManager;
+
+    public OreoNotifications(Context base) {
+        super(base);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            createChannel();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createChannel() {
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT);
+        channel.enableLights(true);
+        channel.enableVibration(false);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(channel);
+    }
+
+    public NotificationManager getManager(){
+        if(notificationManager == null){
+            notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return notificationManager;
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public Notification.Builder getOreoNotification(String title, String body, String icon,
+                                                    PendingIntent pendingIntent, Uri soundUri){
+        return new Notification.Builder(getApplicationContext(), CHANNEL_ID)
+                .setContentIntent(pendingIntent)
+                .setContentText(body)
+                .setContentTitle(title)
+                .setSound(soundUri)
+                .setSmallIcon(Integer.parseInt(icon))
+                .setAutoCancel(true);
+    }
+}
+
