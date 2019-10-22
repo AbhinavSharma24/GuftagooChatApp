737c77ba23e5a8f5e75535915d9b8e2b4bdb477a
abhinav <abhinavsharma24999@gmail.com>
2019-10-22 22:04:10 +0530

    Few Updates

--- app/src/main/java/com/example/guftagoochatapp/Fragments/ChatsFragment.java
+++ app/src/main/java/com/example/guftagoochatapp/Fragments/ChatsFragment.java
@@ -1,6 +1,5 @@
 package com.example.guftagoochatapp.Fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
--- app/src/main/java/com/example/guftagoochatapp/Fragments/UsersFragment.java
+++ app/src/main/java/com/example/guftagoochatapp/Fragments/UsersFragment.java
@@ -67,7 +67,6 @@ public class UsersFragment extends Fragment {
 
             }
         });
-
         return view;
     }
 
